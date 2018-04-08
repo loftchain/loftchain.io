7d2dece932e3fd473b1c400dd73ecb66226bf255
kostalavista <paradoxnumber7@gmail.com>
2018-04-08 21:43:36 +0300

    Texts servises update

--- lang/en.php
+++ lang/en.php
@@ -1,5 +1,5 @@
 <?php
-define('TITLE', 'LoftChain - Blockchain development');
+define('TITLE', 'LoftChain - ICO technical solutions');
 define('HOME', 'Home');
 define('SERVICES', 'Services');
 define('TEAM', 'Team');
@@ -7,44 +7,46 @@ define('CONTACTS', 'Contacts');
 define('DESCRIPTION', 'Blockchain development');
 
 define('SERVICE1_TITLE', 'ICO site development');
-define('SERVICE1_PRICE', 'from $9000');
-define('SERVICE1_DESC', 'First thing that attracts attention of the potential investor is design of the site.
-    <br/><br/>We are developing a specialized design for the blockchain industry, which will increase the credibility of your ICO project.
-    <br/><br/>Our team guarantees clean code using modern frameworks and suitable programming patterns, correct display of the site on any devices and in all browsers.');
+define('SERVICE1_PRICE', 'from $1700');
+define('SERVICE1_DESC', 'The first thing that attracts attention of the potential investor is design of the site.
+    <br/><br/>We develop design that is specialized for the blockchain industry, which will attract the maximum number of potential investors and will help them to easily navigate within the project.
+    <br/><br/>The second is the level of confidence in the project. Our team guarantees clean code using modern frameworks and suitable programming patterns, correct display of the site on any devices and in all browsers. Providing full and consistent information.');
 define('SERVICE1_BENEFITS', '<li>Creation of a logo and corporate identity of the project</li>
-    <li>Unique site design</li>
-    <li>Front-end development</li>
-    <li>Back-end development</li>
-    <li>Investor`s private office</li>
-    <li>Smart contract development</li>');
+    <li>Design development</li>
+    <li>Front-end</li>
+    <li>Back-end</li>
+    <li>Connecting Analytical Systems</li>
+    <li>Marketing copywriting</li>');
 define('SERVICE2_TITLE', 'Private office');
-define('SERVICE2_PRICE', 'from $5000');
+define('SERVICE2_PRICE', 'from $3500');
 define('SERVICE2_DESC', 'Investor`s private office is a safe and convenient fund-raising tool for your ICO project.
     <br/><br/>Functionality of the private office will allow your investors to receive information about conducted transactions and transfer of tokens at the time of the completion of ICO. 
     <br/><br/>Investors will be able to quickly calculate the cost of the token, participate in the referral program, be the first to learn about bonuses and promotions, be aware of stages of ICO.');
 define('SERVICE2_BENEFITS', '<li>Complete crowdsale statistics</li>
     <li>Token payments via BTC, ETH, LTC, etc.</li>
-    <li>Integration with external resources</li>
-    <li>Smart contract development</li>');
+    <li>Integration with external resources</li>');
 
-define('SERVICE3_TITLE', 'Smart contract');
+define('SERVICE3_TITLE', 'Blockchain segment');
 define('SERVICE3_PRICE', 'from $3500');
-define('SERVICE3_DESC', 'Smart contract technology allows you to perform reliable and confidential transactions without the involvement of external intermediaries.
-    <br/><br/>Using a smart contract, you get a convenient ICO tool that will help you to invest in cryptocurrency in a reliable and safe way, reduce the threat of hacker attacks, and substitute information about transactions.');
-define('SERVICE3_BENEFITS', '<li>Project technical infrastructure development, in particular, the system of internal Ethereum (ETH) wallets.</li>
-    <li>Harmonization of technical requirements and business logic</li>
-    <li>Testing and launching wallets system and payment solutions</li>
-    <li>Emission and deployment token to the blockchain</li>');
+define('SERVICE3_DESC', 'After the advent of Ethereum technology, the blockchain environment is capable of executing program code. One of the main applications of this technology is crowdfunding. Initial coin offering (ICO) - involves the use of <b>smart-contracts</b> that allow receiving investments in cryptocurrency in a reliable and safe way, bring to zero the threat of hacker and transaction substitution attacks.');
+define('SERVICE3_BENEFITS', '<li>Development of a smart contract of any complexity</li>
+    <li>Smart contract audit</li>
+    <li>Development lotteries of all kinds</li>
+    <li>Online Games</li>');
 
-define('SERVICE4_TITLE', 'Smart contract audit');
-define('SERVICE4_PRICE', 'from $1500');
-define('SERVICE4_DESC', 'Even minor errors or shortcomings in the work of the logic of the smart contract lead to the loss of large funds.
-    <br/><br/>External audit of the smart contract will help to check the logic of the program, identify errors in the code and prevent (avoid) the vulnerability.
-    <br/><br/>Professional audit will save your funds under reliable protection.');
-define('SERVICE4_BENEFITS', '<li>Vulnerability check</li>
-    <li>Possible errors check</li>
-    <li>Checking Logic and Economics</li>
-    <li>Unit testing</li>');
+define('SERVICE4_TITLE', 'Web segment');
+define('SERVICE4_PRICE', 'from $800');
+define('SERVICE4_DESC', 'Website quality plays a key role in modern business.
+    <br/><br/>We provide full-cycle development and modernisation of websites in line with the best market practices.
+    <br/><br/>By working with us you will receive modern, commercially effective website, demonstrating the advantages of your business.');
+define('SERVICE4_BENEFITS', '<li>Landing page</li>
+    <li>Business card site</li>
+    <li>Promo website</li>
+    <li>Corporate website</li>
+    <li>Online catalog</li>
+    <li>Online store</li>
+    <li>Informational portal</li>
+    <li>Social network</li>');
 
 define('CONTACTS_TEXT1', 'You can write us a message or call:');
 define('CONTACTS_TEXT2', 'Or use alternative ways of communication:');
