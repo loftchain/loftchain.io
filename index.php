2fe91da02fcada4ddc628cfe7d5897569440ac7b
kostalavista <paradoxnumber7@gmail.com>
2018-07-23 11:25:30 +0300

    add HubSpot, remove jivosite

--- index.php
+++ index.php
@@ -96,10 +96,13 @@ $v = 3;
 <!-- /Yandex.Metrika counter -->
 
 <!-- BEGIN JIVOSITE CODE {literal} -->
-<script type='text/javascript'>
-	(function(){ var widget_id = 'GaIpwQ6vtc';var d=document;var w=window;function l(){
-		var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/geo-widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);}if(d.readyState=='complete'){l();}else{if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();</script>
+<!--<script type='text/javascript'>-->
+<!--	(function(){ var widget_id = 'GaIpwQ6vtc';var d=document;var w=window;function l(){-->
+<!--		var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/geo-widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);}if(d.readyState=='complete'){l();}else{if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();</script>-->
 <!-- {/literal} END JIVOSITE CODE -->
+<!-- Start of HubSpot Embed Code -->
+<script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/4755449.js"></script>
+<!-- End of HubSpot Embed Code -->
 
 <div id="page-loader" class="loader-wrap">
 	<div class="loader"></div>
